b10fd3f614124bebb1db592caccfbbf56498402a
Daniel Friedman <danielnfriedman@gmail.com>
2014-10-09 11:57:53 +1100

    ConsolePairingUI: comment required message payload for color and pairing code

--- ConsolePairingUI.go
+++ ConsolePairingUI.go
@@ -9,9 +9,11 @@ type ConsolePairingUI struct {
 }
 
 func (ui *ConsolePairingUI) DisplayColorHint(color string) {
+	// mosquitto_pub -m '{"id":123, "params": [{"color":"#FF0000"}],"jsonrpc": "2.0","method":"displayColor","time":132123123}' -t '$node/:node/led-controller'
 	fmt.Printf(" *** COLOR HINT: %s ***\n", color)
 }
 
 func (ui *ConsolePairingUI) DisplayPairingCode(code string) {
+	// mosquitto_pub -m '{"id":123, "params": [{"code":"1234"}],"jsonrpc": "2.0","method":"displayPairingCode","time":132123123}' -t '$node/:node/led-controller'
 	fmt.Printf(" *** PAIRING CODE: %s ***\n", code)
 }
