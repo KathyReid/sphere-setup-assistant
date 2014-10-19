81b7581540adc03c27dee330a8be55be156ac64b
Theo Julienne <theo.julienne@gmail.com>
2014-10-18 17:17:56 -0700

    Run AP name command using sh.

--- config.go
+++ config.go
@@ -17,7 +17,7 @@ func LoadConfig(path string) AssistantConfig {
 	var cfg AssistantConfig
 
 	// defaults
-	uniqueSuffix, _ := exec.Command("/opt/ninjablocks/bin/sphere-go-serial | sha256sum | cut -c1-8").Output()
+	uniqueSuffix, _ := exec.Command("/bin/sh", "-c", "/opt/ninjablocks/bin/sphere-go-serial | sha256sum | cut -c1-8").Output()
 	cfg.Wireless_Host.SSID = "NinjaSphere-" + string(uniqueSuffix)
 	cfg.Wireless_Host.Key = "ninjasphere"
 	cfg.Wireless_Host.Full_Network_Access = false
