17123e3eb6418c6accde791fc1791761a0495fc5
Jon Seymour <jon@ninjablocks.com>
2014-11-24 18:09:18 +1100

    Restore theo's version of iwlib.
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

--- wifi_setup_rpc.go
+++ wifi_setup_rpc.go
@@ -6,9 +6,9 @@ import (
 	"os/exec"
 	"time"
 
-	"github.com/elliots/go-wireless/iwlib"
 	"github.com/ninjasphere/go-ninja/api"
 	"github.com/ninjasphere/go-ninja/config"
+	"github.com/theojulienne/go-wireless/iwlib"
 )
 
 type WifiNetwork struct {
