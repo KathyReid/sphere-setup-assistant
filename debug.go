189fe44bd87147c74d93a4bdf04e032f7ee1ece5
Mark Wolfe <mark@wolfe.id.au>
2015-02-20 14:46:20 +1100

    Removed old logging setup.

--- debug.go
+++ debug.go
@@ -2,15 +2,9 @@
 
 package main
 
-import (
-	"github.com/bugsnag/bugsnag-go"
-	"github.com/juju/loggo"
-	nlog "github.com/ninjasphere/go-ninja/logger"
-)
+import "github.com/bugsnag/bugsnag-go"
 
 func init() {
-	nlog.GetLogger("").SetLogLevel(loggo.DEBUG)
-
 	bugsnag.Configure(bugsnag.Configuration{
 		APIKey:       "1e2278d66bee6cbd579606e2a0e623f3",
 		ReleaseStage: "development",
